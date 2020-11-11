b5c4113f17149aca02b1f48118f0a2419491c06e
Brax-1 <ay701548@gmail.com>
2020-11-11 20:48:28 +0500

    fixing gap in index.php

--- portfolio/index.php
+++ portfolio/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Intro</title>
     <link rel="stylesheet" href="css/first.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@800&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet">
